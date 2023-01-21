28b093388fab6b36b120822bb150f98175417dc8
Ge Will <531sunlight@gmail.com>
2023-01-21 12:03:43 +0800

    Initial Commit

---
+++ AD Demo/AD_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AD_DemoApp.swift
+//  AD Demo
+//
+//  Created by will on 2023/1/21.
+//
+
+import SwiftUI
+
+@main
+struct AD_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
